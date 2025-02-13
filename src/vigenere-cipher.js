314ec48516ba8892883d744414b95fe4a246f8ee
TheMozii <ruvimungeitis1@gmail.com>
2025-02-13 11:17:40 +0200

    feat: vigenere ciphering machine test done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    const upperMessage = message.toUpperCase();
+    const upperKey = key.toUpperCase();
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i < upperMessage.length; i++) {
+      const char = upperMessage[i];
+      if (char >= 'A' && char <= 'Z') {
+        const mCode = char.charCodeAt(0) - 65;
+        const kCode = upperKey[j % upperKey.length].charCodeAt(0) - 65;
+        const encryptedChar = String.fromCharCode(((mCode + kCode) % 26) + 65);
+        result += encryptedChar;
+        j++;
+      } else {
+        result += char;
+      }
+    }
+    if (!this.isDirect) {
+      result = result.split('').reverse().join('');
+    }
+
+    return result;
+  }
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    
+    const upperMessage = message.toUpperCase();
+    const upperKey = key.toUpperCase();
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i < upperMessage.length; i++) {
+      const char = upperMessage[i];
+      if (char >= 'A' && char <= 'Z') {
+        const cCode = char.charCodeAt(0) - 65;
+        const kCode = upperKey[j % upperKey.length].charCodeAt(0) - 65;
+        const decryptedChar = String.fromCharCode(((cCode - kCode + 26) % 26) + 65);
+        result += decryptedChar;
+        j++;
+      } else {
+        result += char;
+      }
+    }
+
+    if (!this.isDirect) {
+      result = result.split('').reverse().join('');
+    }
+
+    return result;
   }
 }
 
